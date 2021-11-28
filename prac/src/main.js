21dfacae92e24d1b4517a32a63a8910c62f577c8
Gourav Singh Rawat <grvrawat999@gmail.com>
2021-11-28 13:38:54 +0530

    created 404 page

--- prac/src/main.js
+++ prac/src/main.js
@@ -4,7 +4,6 @@ const app = express();
 const port = 8000;
 const path = require("path");
 
-//built-in middleware
 // const staticPath = path.join(__dirname, '../');
 const templatePath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, "../templates/partials/");
@@ -13,16 +12,17 @@ const partialsPath = path.join(__dirname, "../templates/partials/");
 app.set("view engine", "hbs");
 app.set("views", templatePath);
 hbs.registerPartials(partialsPath);
-
-
 // app.use(express.static(staticPath));
 
-app.get("", (req, res) => {
+
+app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+app.get("*", (req, res) => {
+    res.render("404", {
+        errorcomment: "Page not found."
+    })
 });
 
 app.listen(port, () => {
